881d3742aaeb1025f8849b41dae77b74e81ee7e7
baltej223 <baltej963@gmail.com>
2025-08-29 02:33:03 +0530

    JWT genration and verfication works

--- backend/middlewares/Auth.ts
+++ backend/middlewares/Auth.ts
@@ -1,20 +1,10 @@
-import { Request, Response } from "express";
-import { Strategy, ExtractJwt } from "passport-jwt";
-import passport from "passport";
+import { NextFunction, Request, Response } from "express";
+import passport from "../funcs/passportStrategy.ts";
 
-var opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = "somerandomsecrentusingwhichthedatacan";
-
-passport.use(
-  new Strategy(opts, (payload, done) => {
-    console.log(payload);
-    done(null, false);
-  }),
-);
-
-export default function Authorise(req: Request, res: Response, next: any) {
-  let cookie = req.cookies;
-  console.log(cookie);
-  next();
+export default function Authorise(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  passport.authenticate("jwt", { session: false })(req, res, next);
 }
